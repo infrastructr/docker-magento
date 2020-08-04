0492141af7b21bc2bc20e41d9371b833d241b678
mle <michael@lewkowski.de>
2020-08-05 00:25:53 +0200

    Await database connection

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -3,6 +3,15 @@
 set -e
 
 if [ "$1" = "apache2-foreground" ]; then
+    n=0
+    until [ "$n" -ge 5 ]
+    do
+       echo "Waiting for database connection ($n)..."
+       mysqladmin ping -h"${MAGENTO_DB_HOST}" --silent && break
+       n=$((n+1))
+       sleep 2
+    done
+
     bin/magento setup:install \
         --base-url="${MAGENTO_BASE_URL}" \
         --db-host="${MAGENTO_DB_HOST}" \
